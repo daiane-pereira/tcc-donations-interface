8eccf847a6ef92b80a7bc8de30c5f7b6864d88fd
Daiane Pereira Kuwiecinski <daiane.sper@gmail.com>
2020-11-24 21:54:30 -0300

    :wrench: Update configuration to Heroku.

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+//Importa as dependências que acabamos de instalar
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
+
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + '/dist/donation-ui'));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/donation-ui/index.html'));
 });
 
+// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
