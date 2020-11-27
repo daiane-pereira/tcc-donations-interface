8aa2d0e7c6a75a461211908fffcd71991924f17c
daiane-pereira <daiane.sper@gmail.com>
2020-11-26 21:03:41 -0300

    Habilitado tela de detalhes da doação, porém sem os contatos. (#7)

--- src/app/doacoes/doacoes-routing.module.ts
+++ src/app/doacoes/doacoes-routing.module.ts
@@ -24,7 +24,6 @@ const routes: Routes = [
       },
       {
         path: 'detalhes/:doacaoId',
-        canActivate: [AuthGuard],
         component: ConsultaDetalhesDoacaoComponent,
       },
       {
