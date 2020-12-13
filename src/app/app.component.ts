b9a5a66aabf2a296d53b304f700c72e46b37c36c
Daiane Pereira Kuwiecinski <daiane.sper@gmail.com>
2020-12-13 18:57:55 -0300

    Removido Meta e Title do index.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +7,7 @@ import {Meta, Title} from '@angular/platform-browser';
 })
 export class AppComponent implements OnInit{
 
-  constructor(
-    private titleService: Title,
-    private metaTagService: Meta
-  ) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.titleService.setTitle('Move a Roda');
-
-    this.metaTagService.addTags([
-      {name: 'og:title', content: 'Move a Roda — Doe e receba doações'},
-      {name: 'og:description', content: 'Fazer uma doação é um ato benéfico para a sociedade e para o meio ambiente. Realize uma doação' +
-          ' um pertence que não utilizam mais, ou busque por objetos, roupas e outros produtos que estejam sendo cedidos.'}
-    ]);
-  }
+  ngOnInit() {}
 }
